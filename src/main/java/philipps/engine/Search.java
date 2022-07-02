3b68148c6a4338315282bd526d15a1e28297b989
David Weingut <da.we_2003@web.de>
2022-07-03 00:42:10 +0200

    various small fixes fix directory tree, add gitignore, fix AB negamax

--- src/main/java/philipps/engine/Search.java
+++ src/main/java/philipps/engine/Search.java
@@ -7,7 +7,7 @@ import com.github.bhlangonijr.chesslib.Side;
 import com.github.bhlangonijr.chesslib.move.Move;
 
 public class Search {
-   public static int negamax(Board board, int depth, int ply, int alpha, int beta) {
+   public static int negamax(Board board, int depth, int ply, int alpha , int beta) {
       List<Move> moves = board.legalMoves();
       if (depth <= 0 || moves.isEmpty()) {
          return Evaluation.evaluate(board, depth);
@@ -17,14 +17,16 @@ public class Search {
       int bestEval = Integer.MIN_VALUE;
       for (Move m : moves) {
          board.doMove(m);
-         int eval = -negamax(board, depth - 1, ply + 1, -beta ,-alpha);
+         int eval = -negamax(board, depth - 1, ply + 1, -beta, -alpha);
          board.undoMove();
          if (eval >= beta) {
             return beta;
          }
-         if ( eval > alpha ) {
+         if (eval > alpha) {
             alpha = eval;
          }
+         if (eval > bestEval){
+            bestEval = eval;
          }
       }
       return alpha;
@@ -33,13 +35,13 @@ public class Search {
    public static Move TLnegamax(Board board, int depth, int ply, int returnScore[]) {
       List<Move> moves = board.legalMoves();
       if (depth <= 0 || moves.isEmpty()) {
-         return new Move("a2a4", Side.WHITE); 
+         return new Move("a2a4", Side.WHITE);
       }
       int bestEval = Integer.MIN_VALUE;
       Move bestMove = moves.get(0);
       for (Move m : moves) {
          board.doMove(m);
-         int eval = -negamax(board, depth - 1, ply + 1);
+         int eval = -negamax(board, depth - 1, ply + 1, -Integer.MAX_VALUE, Integer.MAX_VALUE);
          board.undoMove();
          if (eval > bestEval) {
             bestEval = eval;
